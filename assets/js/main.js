c145c2e85dc6d98ba147daae86e0e0050fbc44dc
Lucas <contatolucasm9@gmail.com>
2023-01-08 13:27:32 -0300

    Optimização em funções e alterações CSS

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,16 @@ let senhaNormal = 0;
 let senhaPreferencial = 0;
 let ultSenha = 0;
 let audio = new Audio('./audio/senha.mp3');
+let senhas = document.getElementById('ultSenhas');
+let senhasChamadas = [];
+let consult = document.getElementById('consultorio');
+let consultorios = [];
+let pacientes = [];
+
+
+for (let i = 1; i < 6; i++) {
+    consultorios.push(i);
+}
 
 if (!senhaNormal)
     senhaNormal = 0
@@ -11,7 +21,7 @@ if (!senhaPreferencial)
     senhaPreferencial = 0
 
 if (!ultSenha)
-    ultSenha = 'N'
+    ultSenha = "N"
 
 mostrarSenha();
 
@@ -21,15 +31,25 @@ btn.addEventListener('click', function(e){
     
 })
 
+
+// função de chamada de senhas normais
 function sNormal() {
     const name = document.getElementById('Name').value;
-    document.getElementById('nome').innerHTML = `${name}`; 
+    document.getElementById('nome').innerHTML = `${name}`;
+    
     ultSenha = 'N';
     senhaNormal++;
     audio.play(); 
     mostrarSenha();
+
+    if (consultorios.length > 0) {
+        consult.innerHTML += `${consultorios.shift()}`
+    } else {
+        consult.innerHTML += `Consultórios ocupados!`
+    }
 }
 
+// função de chamada de senha preferencial
 function sPreferencial() {
     const sPref = document.getElementById('Name').value;
     document.getElementById('nome').innerHTML = `${sPref}`;
@@ -37,15 +57,26 @@ function sPreferencial() {
     senhaPreferencial++;
     audio.play()
     mostrarSenha();
+
+    if (consultorios.length > 0) {
+        consult.innerHTML += `${consultorios.shift()}`
+    } else {
+        consult.innerHTML += `Consultórios ocupados!`
+    }
 }
 
-
-
+// função de vizualização de senhas
 function mostrarSenha() {
+    senhasChamadas.unshift(vSenha.innerHTML);
+    senhasChamadas = senhasChamadas.slice(0,5);
+    document.getElementById('ultSenhas').innerHTML = senhasChamadas.join('<br>'); 
+    
+
     if (ultSenha=='N'){
         vSenha.innerHTML = 'N' + parseInt(senhaNormal).toLocaleString('pt-br' , {minimumIntegerDigits:3});
     } else {
         vSenha.innerHTML = 'P' + parseInt(senhaPreferencial).toLocaleString('pt-br', {minimumIntegerDigits:3})
     }
+    console.log(`${senhasChamadas}`);
 }
 
